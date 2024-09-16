06900fb2af2269be8e339f5f30cc24fa6dfc0399
Artem <bezkorovainy2006@gmail.com>
2024-09-16 17:36:25 +0200

    updated tailwind configs, add postcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 
 const App = () => {
   return (
-    <div>
+    <div className="">
       AI-CHAT-APP!
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",  // Все файлы внутри директории src
+    ],
     theme: {
       extend: {},
     },
     plugins: [],
   }
+  
